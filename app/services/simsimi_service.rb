9f762f7d30eb13d0399a1c48c5eb311405a5de0f
Puxuan He <puxuanhe@gmail.com>
2016-04-16 16:56:43 -0400

    add default message

---
+++ app/services/simsimi_service.rb
@@ -0,0 +1,12 @@
+require "net/http"
+require 'open-uri'
+require "uri"
+require 'json'
+class SimSimiService
+	@@API_KEY = 'f765df6a-112f-4f20-ae65-424467831991'
+	def getResponse(message)
+		uri = URI.parse("http://sandbox.api.simsimi.com/request.p?key=#{@@API_KEY}&lc=en&ft=1.0&text=#{URI::encode(message)}")
+    	response = Net::HTTP.get_response(uri)
+    	return JSON.parse response.body
+	end
+end
