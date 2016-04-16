a4a23a830798f538f2b02671b69ca95e8fdefffe
Puxuan He <puxuanhe@gmail.com>
2016-04-16 16:54:10 -0400

    add default message

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,13 +1,25 @@
 require "net/http"
 require 'open-uri'
 require "uri"
-class MessageController < ApplicationController
-  def get
-    api_key = 'f765df6a-112f-4f20-ae65-424467831991'
-    puts params[:message]
-    uri = URI.parse("http://sandbox.api.simsimi.com/request.p?key=#{api_key}&lc=en&ft=1.0&text=#{URI::encode(params[:message])}")
+require 'json'
+require "#{Rails.root}/app/services/simsimi_service.rb"
 
-    response = Net::HTTP.get_response(uri)
-    render json: response.body
+class MessageController < ApplicationController
+  @@simsimi_service = SimSimiService.new()
+  
+  def get
+    default_response_list = [
+      'mew!mew!mew',
+      '喵喵喵!',
+      'wait a sec, GIVE ME TREATS and then I\'ll talk to you',
+      'Dn\'t care, I need to get some sleep'
+    ]
+
+    response = @@simsimi_service.getResponse(params[:message])
+    if !response[:response]
+       response[:response] = default_response_list[rand(default_response_list.size)]
+    end
+
+    render json: {response: response[:response]}
   end
 end
